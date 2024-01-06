7da05ce55f0960bfeee2da3f17e3a87bf0eb4a0e
Regina Liu <regina.liuyixuan@gmail.com>
2024-01-07 03:26:34 +0800

    Update

--- quartz/plugins/filters/explicit.ts
+++ quartz/plugins/filters/explicit.ts
@@ -3,7 +3,15 @@ import { QuartzFilterPlugin } from "../types"
 export const ExplicitPublish: QuartzFilterPlugin = () => ({
   name: "ExplicitPublish",
   shouldPublish(_ctx, [_tree, vfile]) {
-    const publishFlag: boolean = vfile.data?.frontmatter?.publish ?? false
+    const publishFlag: boolean = convertStringToBoolean(vfile.data?.frontmatter?.publish) ?? false
     return publishFlag
   },
 })
+
+// Helpers
+
+const convertStringToBoolean = (value: string) => {
+  if (value === "true") return true
+  if (value === "false") return false
+  return undefined
+}
